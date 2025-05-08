310ecb2a6465ef3530a4c5e2ef73c0ee89e8909d
surajambrale <surajambrale9003@gmail.com>
2025-05-08 21:15:44 +0530

    nothing changes bass spec file ko comment kiya he

--- src/app/adminlogin/adminlogin.component.spec.ts
+++ src/app/adminlogin/adminlogin.component.spec.ts
@@ -1,23 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AdminloginComponent } from './adminlogin.component';
+// import { AdminloginComponent } from './adminlogin.component';
 
-describe('AdminloginComponent', () => {
-  let component: AdminloginComponent;
-  let fixture: ComponentFixture<AdminloginComponent>;
+// describe('AdminloginComponent', () => {
+//   let component: AdminloginComponent;
+//   let fixture: ComponentFixture<AdminloginComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AdminloginComponent]
-    })
-    .compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [AdminloginComponent]
+//     })
+//     .compileComponents();
 
-    fixture = TestBed.createComponent(AdminloginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(AdminloginComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
